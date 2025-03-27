dcc8275203bdda1588d32e11c333e07453ada809
akiidjk <dragonfaiver@gmail.com>
2025-03-27 20:35:02 +0100

    Add missing slash in middleware API routes

--- adh-dashboard/src/middleware.ts
+++ adh-dashboard/src/middleware.ts
@@ -2,11 +2,12 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { decrypt } from "@/lib/session";
 
-const protectedRoutes = ["/", "api/delete", "api/history", "api/stream"];
+const protectedRoutes = ["/", "/api/delete", "/api/history", "/api/stream"];
 const publicRoutes = ["/login"];
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
+
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
 
